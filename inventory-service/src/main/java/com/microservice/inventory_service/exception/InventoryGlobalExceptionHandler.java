0dc93bff0de7daaa055faca5cc3efdaa81d1d6ad
Abhijeet Kumar Bhagat <abhijeet@vrize.com>
2026-01-27 10:21:43 +0530

    Deployed on Deocker

--- inventory-service/src/main/java/com/microservice/inventory_service/exception/InventoryGlobalExceptionHandler.java
+++ inventory-service/src/main/java/com/microservice/inventory_service/exception/InventoryGlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.microservice.inventory_service.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
